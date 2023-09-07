929784653778876f1baf8ad4ba43b084bef82c9b
soO <444deph12@gmail.com>
2023-09-07 08:38:06 +0000

    fixing PEP8

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,13 +3,15 @@
 min_operation.py
 """
 
+
 def minOperations(n):
-	"""
-	Calculates the fewest number of operations needed to result in exactly n H characters in the file.
-	"""
-	if n <= 1:
-		return 0
-	else:
-		for i in range(2, n + 1):
-			if n % i == 0:
-				return minOperations(int(n / i)) + i
+    """
+    Calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+    else:
+        for i in range(2, n + 1):
+            if n % i == 0:
+                return minOperations(int(n / i)) + i
