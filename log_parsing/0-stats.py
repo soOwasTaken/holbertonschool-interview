b20a97ea73603ca6b9445775f292b8f43599b4a8
soO <444deph12@gmail.com>
2023-09-26 12:25:59 +0000

    doc

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,81 +1,39 @@
 #!/usr/bin/python3
 """
-stats.py Module
-
-This module provides functionality to compute metrics from web server log entries that are fed from stdin.
-
-Metrics computed:
-1. Total file size across all requests.
-2. Count of requests for each HTTP status code.
-
-The expected format for each log entry:
-IP_ADDRESS - - [TIMESTAMP] "REQUEST" STATUS_CODE FILE_SIZE
-e.g., 127.0.0.1 - - [26/Sep/2023:14:55:36 -0700] "GET /index.html HTTP/1.1" 200 2326
-
-After processing every 10 lines, the module prints the metrics to stdout. If interrupted by a keyboard signal (like Ctrl+C),
-the current metrics will be printed.
-
-Usage:
-Pipe log entries into the script. For example:
-$ cat server.log | ./stats.py
-
-Dependencies:
-- sys
-- collections.defaultdict
-
+Log parsing
 """
 
 import sys
-from collections import defaultdict
 
+if __name__ == '__main__':
 
-def compute_metrics():
-    """
-    Reads stdin line by line and computes metrics for server logs.
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-    This function processes each line, extracts relevant metrics, and prints results after every 10 lines.
-    In the case of a KeyboardInterrupt, the current metrics will also be printed.
-    """
-    total_size = 0
-    status_counts = defaultdict(int)
-    line_count = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            parts = line.split()
-
-            if len(parts) >= 7:
-                status_code = parts[-2]
-                file_size_str = parts[-1]
-
-                if status_code.isdigit():
-                    status_code = int(status_code)
-                    file_size = int(file_size_str)
-
-                    total_size += file_size
-                    status_counts[status_code] += 1
-                    line_count += 1
-
-                    if line_count % 10 == 0:
-                        print_metrics(total_size, status_counts)
-
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_metrics(total_size, status_counts)
-
-
-def print_metrics(total_size, status_counts):
-    """
-    Prints the current server log metrics to stdout.
-
-    Args:
-    - total_size (int): Total file size accumulated so far.
-    - status_counts (dict): Dictionary containing counts of each HTTP status code.
-    """
-    print("Total file size:", total_size)
-    for code in sorted(status_counts.keys()):
-        print(f"{code}: {status_counts[code]}")
-    print()
-
-
-if __name__ == "__main__":
-    compute_metrics()
+        print_stats(stats, filesize)
+        raise
