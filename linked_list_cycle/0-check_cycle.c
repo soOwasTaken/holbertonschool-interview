422136064b4295772ab470baece8083ae2109bd4
soO <444deph12@gmail.com>
2023-10-06 05:22:01 +0000

    linked_list_cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: points to the head of the linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *tortoise = list;
+    listint_t *hare = list;
+
+    if (!list)
+        return (0);
+
+    while (hare && hare->next)
+    {
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        if (tortoise == hare)
+            return (1);
+    }
+
+    return (0);
+}
