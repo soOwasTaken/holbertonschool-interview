93477bc458df0a276c2c7b7f1ebb8700feb827aa
soO <444deph12@gmail.com>
2023-12-01 14:43:47 +0000

    Linear Skip

---
+++ linear_skip/0-linear_skip.c
@@ -0,0 +1,46 @@
+#include "search.h"
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * linear_skip - searches for a value in a sorted skip list of integers
+ * @list: pointer to the head of the skip list to search in
+ * @value: value to search for
+ *
+ * Return: pointer on the first node where value is located
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    skiplist_t *node, *express;
+
+    if (list == NULL)
+        return (NULL);
+
+    express = list->express;
+    node = list;
+
+    // Traverse through the express lane
+    while (express && express->n < value) {
+        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+        node = express;
+        express = express->express;
+    }
+
+    if (express)
+        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+    else
+        for (express = node; express->next; express = express->next);
+
+    printf("Value found between indexes [%lu] and [%lu]\n", node->index, express->index);
+
+    // Traverse through the normal lane
+    while (node && node->n < value) {
+        printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+        node = node->next;
+    }
+
+    if (node && node->n == value)
+        printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+
+    return (node && node->n == value) ? node : NULL;
+}
