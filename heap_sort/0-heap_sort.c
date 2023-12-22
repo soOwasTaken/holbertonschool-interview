723db083444eba4c196a061b2baaada5b711781b
soO <444deph12@gmail.com>
2023-12-22 11:48:19 +0000

    heap sort

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -7,9 +7,9 @@
  */
 void swap(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -21,22 +21,22 @@ void swap(int *a, int *b)
  */
 void heapify(int *array, size_t size, int i, size_t total_size)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
 
-    if (left < (int)size && array[left] > array[largest])
-        largest = left;
+	if (left < (int)size && array[left] > array[largest])
+		largest = left;
 
-    if (right < (int)size && array[right] > array[largest])
-        largest = right;
+	if (right < (int)size && array[right] > array[largest])
+		largest = right;
 
-    if (largest != i)
-    {
-        swap(&array[i], &array[largest]);
-        print_array(array, total_size);
-        heapify(array, size, largest, total_size);
-    }
+	if (largest != i)
+	{
+		swap(&array[i], &array[largest]);
+		print_array(array, total_size);
+		heapify(array, size, largest, total_size);
+	}
 }
 
 /**
@@ -46,19 +46,19 @@ void heapify(int *array, size_t size, int i, size_t total_size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
+	int i;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, size, i, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		heapify(array, size, i, size);
 
-    for (i = size - 1; i >= 0; i--)
-    {
-        swap(&array[0], &array[i]);
-        if (i != 0)
-            print_array(array, size);
-        heapify(array, i, 0, size);
-    }
+	for (i = size - 1; i >= 0; i--)
+	{
+		swap(&array[0], &array[i]);
+		if (i != 0)
+			print_array(array, size);
+		heapify(array, i, 0, size);
+	}
 }
