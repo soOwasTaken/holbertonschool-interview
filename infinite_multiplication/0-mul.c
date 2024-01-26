16602eed6e8bf90c74189cdb36567827f123b9f1
soO <444deph12@gmail.com>
2024-01-26 10:48:50 +0000

    fixing

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,44 +1,92 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
 
 /**
- * _isNumber - checks if a string is a number
- * @str: string to check
- * Return: 1 if number, 0 otherwise
+ * _isnumber - checks if string is number
+ * @s: string
+ * Return: 1 if number, 0 if not
  */
-int _isNumber(char *str) {
-	for (int i = 0; str[i] != '\0'; i++) {
-		if (!isdigit(str[i]))
+int _isnumber(char *s)
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		if (!isdigit(s[i]))
 			return 0;
 	}
 	return 1;
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 98 on error
+ * _callocX - reserves memory initialized to 0
+ * @nmemb: # of bytes
+ * Return: pointer
  */
-int main(int argc, char *argv[]) {
-	long num1, num2, result;
+char *_callocX(unsigned int nmemb)
+{
+	char *p = malloc(nmemb + 1);
+	if (p == NULL)
+		return NULL;
 
-	if (argc != 3) {
+	for (unsigned int i = 0; i < nmemb; i++)
+		p[i] = '0';
+	p[nmemb] = '\0';
+	return p;
+}
+
+/**
+ * main - multiplies inf numbers
+ * @argc: # of cmd line args
+ * @argv: cmd line args
+ * Return: No return
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+	{
 		printf("Error\n");
-		return 98;
+		exit(98);
 	}
 
-	if (!_isNumber(argv[1]) || !_isNumber(argv[2])) {
-		printf("Error\n");
-		return 98;
+	int num1_zero = atoi(argv[1]) == 0;
+	int num2_zero = atoi(argv[2]) == 0;
+	if (num1_zero || num2_zero)
+	{
+		printf("0\n");
+		exit(0);
 	}
 
-	num1 = atol(argv[1]);
-	num2 = atol(argv[2]);
-	result = num1 * num2;
+	int len1 = strlen(argv[1]);
+	int len2 = strlen(argv[2]);
+	int full_len = len1 + len2;
+	char *result = _callocX(full_len);
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	printf("%ld\n", result);
+	for (int i = len2 - 1; i >= 0; i--)
+	{
+		int carry = 0;
+		for (int j = len1 - 1; j >= 0; j--)
+		{
+			int temp_loc = i + j + 1;
+			int product = (argv[1][j] - '0') * (argv[2][i] - '0') + carry;
+			carry = product / 10;
+			int sum = (result[temp_loc] - '0') + (product % 10);
+			result[temp_loc] = (sum % 10) + '0';
+			carry += sum / 10;
+		}
+		result[i] = carry + '0';
+	}
+
+	int start = result[0] == '0' ? 1 : 0;
+	for (int i = start; i < full_len; i++)
+		printf("%c", result[i]);
+	printf("\n");
+
+	free(result);
 	return 0;
 }
