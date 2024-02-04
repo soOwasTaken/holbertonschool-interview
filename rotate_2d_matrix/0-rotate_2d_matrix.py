a0b296d8bf69166cc0081fe6e39336697aa1aa04
soO <444deph12@gmail.com>
2024-02-04 12:46:37 +0000

    rotate 2d matrix

---
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Test  - Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
