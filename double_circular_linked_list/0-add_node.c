dbe8a7083b93c5585b7463ab283c0d4bee8f1011
soOwasTaken <444deph12@gmail.com323>
2024-03-08 10:39:24 +0100

    fixing scenarios

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -12,38 +12,36 @@
 
  */
 
-List *add_node_end(List **list, char *str)
-{
-  List *new_node = malloc(sizeof(List));
-  List *last = *list;
+List *add_node_end(List **list, char *str) {
+    List *new_node = malloc(sizeof(List));
+    if (!new_node)
+        return (NULL);
 
-  if (!new_node)
-    return(NULL);
+    new_node->str = strdup(str);
+    if (!new_node->str) {
+        free(new_node);
+        return (NULL);
 
-  new_node->str = strdup(str);
-  if (!new_node->str) {
-    free(new_node);
-    return(NULL);
-  }
+    }
 
-  new_node->next = *list;
-  if (*list) {
-    while (last->next != *list)
-      last = last->next;
-    last->next = new_node;
-  } else {
-    new_node->next = new_node;
-  }
-  new_node->prev = last;
-  if (*list)
-    (*list)->prev = new_node;
-  *list = new_node;
+    if (*list == NULL) {
+        new_node->next = new_node;
+        new_node->prev = new_node;
 
-  return (new_node);
+        *list = new_node;
+    } else {
+        new_node->next = *list;
+
+        new_node->prev = (*list)->prev;
+        (*list)->prev->next = new_node;
+
+        (*list)->prev = new_node;
+    }
+
+    return (new_node);
 }
 
 /**
-
  * add_node_begin - Add a new node to the beginning of a double circular linked list
 
  * @list: Pointer to the head of the linked list
