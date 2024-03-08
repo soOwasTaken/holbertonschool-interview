33f4d37731824a6fc59bf213b155ad2d7e025f8e
soOwasTaken <444deph12@gmail.com323>
2024-03-08 11:08:25 +0100

    fixing betty

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -12,24 +12,29 @@
 
  */
 
-List *add_node_end(List **list, char *str) {
+List *add_node_end(List **list, char *str)
+{
 	List *new_node = malloc(sizeof(List));
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str) {
+	if (!new_node->str)
+	{
 		free(new_node);
 		return (NULL);
 
 	}
 
-	if (*list == NULL) {
+	if (*list == NULL)
+	{
 		new_node->next = new_node;
 		new_node->prev = new_node;
 
 		*list = new_node;
-	} else {
+	} else
+	{
 		new_node->next = *list;
 
 		new_node->prev = (*list)->prev;
@@ -42,29 +47,34 @@ List *add_node_end(List **list, char *str) {
 }
 
 /**
- * add_node_begin - Add a new node to the beginning of a double circular linked list
+ * add_node_begin - Add a new node to the beginning of a double clk
 
  * @list: Pointer to the head of the linked list
  * @str: The string to copy into the new node
  *
  * Return: Address of the new node, or NULL on failure
  */
-List *add_node_begin(List **list, char *str) {
+List *add_node_begin(List **list, char *str)
+{
 	List *new_node = malloc(sizeof(List));
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	if (!new_node->str) {
+	if (!new_node->str)
+	{
 		free(new_node);
 		return (NULL);
 	}
 
-	if (*list == NULL) {
+	if (*list == NULL)
+	{
 		new_node->next = new_node;
 		new_node->prev = new_node;
 		*list = new_node;
-	} else {
+	} else
+	{
 		new_node->next = *list;
 		new_node->prev = (*list)->prev;
 		(*list)->prev->next = new_node;
